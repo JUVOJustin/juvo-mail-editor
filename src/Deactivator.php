e7512f1e7412722e0b43b27d6eeba5f42cf2d06d
Justin Vogt <mail@juvo-design.de>
2020-11-19 18:07:40 +0100

    Remove Notices on deactivation

--- src/Deactivator.php
+++ src/Deactivator.php
@@ -3,9 +3,14 @@
 namespace JUVO_MailEditor;
 
 
+use juvo\WordPressAdminNotices\Manager;
+
 class Deactivator {
 
 	public static function deactivate() {
+
+		// Remove all Notices on Deactivation
+		Manager::remove( "juvo_mail_editor_missing_plugin");
 
 	}
 

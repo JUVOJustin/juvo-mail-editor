b96994f83c39d0b010b38cacd277a805867a42d2
Justin Vogt <mail@justin-vogt.de>
2022-02-03 23:43:52 +0100

    Make recipients an array, add attachments

--- src/Mails/Generic.php
+++ src/Mails/Generic.php
@@ -9,25 +9,29 @@ class Generic {
 	private $content = '';
 	private $recipients;
 	private $headers;
+	private $attachments;
 
 	/**
 	 * Password_Reset constructor.
 	 *
 	 * @param string $subject
 	 * @param string $content
-	 * @param string $recipients
+	 * @param array $recipients
+	 * @param array $headers
+	 * @param array $attachments
 	 */
-	public function __construct( string $subject, string $content, string $recipients, array $headers ) {
-		$this->subject    = $subject;
-		$this->content    = $content;
-		$this->recipients = $recipients;
-		$this->headers    = $headers;
+	public function __construct( string $subject, string $content, array $recipients, array $headers, array $attachments ) {
+		$this->subject     = $subject;
+		$this->content     = $content;
+		$this->recipients  = $recipients;
+		$this->headers     = $headers;
+		$this->attachments = $attachments;
 	}
 
 	/**
 	 * Wrapper for wp_mail
 	 */
 	public function send() {
-		return wp_mail( $this->recipients, $this->subject, $this->content, $this->headers );
+		return wp_mail( $this->recipients, $this->subject, $this->content, $this->headers, $this->attachments );
 	}
 }
--- src/Mails/Mail.php
+++ src/Mails/Mail.php
@@ -4,11 +4,11 @@ namespace JUVO_MailEditor\Mails;
 
 interface Mail {
 
-	public function getSubject(): string;
+	public function getSubject( string $subject ): string;
 
-	public function getMessage(): string;
+	public function getMessage( string $message ): string;
 
-	public function getRecipient(): string;
+	public function getRecipients( array $recipients ): array;
 
 	public function getAlwaysSent(): bool;
 

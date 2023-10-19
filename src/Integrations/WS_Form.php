ff69fe25b9998d4b17947d03165886fd0e57fff4
Justin Vogt <mail@justin-vogt.de>
2023-10-19 21:47:32 +0200

    Initial Work

---
+++ src/Integrations/WS_Form.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace JUVO_MailEditor\Integrations;
+
+class WS_Form {
+
+	public function header_filter_callback(array $headers, $form, $submit, array $action) {
+
+		$actions = $form->meta->action->groups[0]->rows;
+
+		foreach ($actions as $action) {
+
+		}
+
+
+		return $headers;
+
+	}
+
+	private function forgot_password() {
+
+	}
+
+}
